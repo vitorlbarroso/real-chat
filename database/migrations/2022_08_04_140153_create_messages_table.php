75c3bcd9fd8944fca55a95a88432372215ea5ceb
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-08-04 11:55:40 -0300

    Modelada a entidade de mensagens [Message]

---
+++ database/migrations/2022_08_04_140153_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('from')->constrained('users')->onDelete('cascade');
+            $table->foreignId('to')->constrained('users')->onDelete('cascade');
+            $table->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
